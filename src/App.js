4a412e87d796ef40b226b41cb35827829bcb8d98
fadaei <mohammad4252@gmail.com>
2020-09-27 15:18:29 +0330

    last commit with a little change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Fragment,useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { ToastContainer } from 'react-toastify';
 import AppContex from './AppContex';
 import Jobs from './component/jobs/Jobs';
-import NotHaveJob from './component/jobs/NotHaveJob';
 import FormAddJob from './component/Form';
 import "./app.css";
 import './bootstrap-temp.css';
@@ -10,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const context=useContext(AppContex);
+  const context = useContext(AppContex);
   return (
 
     <Fragment>
@@ -19,11 +18,13 @@ const App = () => {
         <div className='wrapper'>
           <div className='app'>
             {
-              context.jobs.length>0 ? (
+              context.jobs.length > 0 ? (
                 <Jobs jobs={context.jobs} />
               ) : (
-                <NotHaveJob />
-              )
+                  <div className="not-have-job">
+                    کاری برای انجام نیست دوست عزیز
+                  </div>
+                )
             }
             {/* <Jobs jobs={context.jobs} /> */}
             {/* <NotHaveJob/> */}
