264a9c57dfe4f5ae9a3c9e05afc65fc5f6eb2a7f
fadaei <mohammad4252@gmail.com>
2020-09-27 11:28:16 +0330

    adding global state to project 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom'
 import App from './App';
+import GlobalState from './component/GlobalState';
 
-render( < App /> , document.getElementById("root"));
+render(
+< GlobalState >
+    <App />
+</GlobalState>
+    , document.getElementById("root"));
